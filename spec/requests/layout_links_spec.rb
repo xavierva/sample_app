733f5c8cad1fe7d322236e1d6ff830da0ade7cae
Xavier <vonaarburgx@gmail.com>
2012-12-17 00:43:57 +0100

    layout tests

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+    it "should find a home page  at '/" do
+      get '/'
+      response.status have_selector('title', :content => "Accueil")
+    end
+    
+    it "should find a contact page at /contact" do
+      get '/contact'
+      response.should have_selector('title', :content => "Contact")
+    end
+    
+    it "should find a contact page at /about" do
+      get '/about'
+      response.should have_selector('title', :content => "À Propos")
+    end
+    
+    it "should find a contact page at /help" do
+      get '/help'
+      response.should have_selector('title', :content => "Aide")
+    end
+    
+end
