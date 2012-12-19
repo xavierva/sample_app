03f49b29e7b685dd900c367d2f9ea8d31d0cbc0d
Xavier <vonaarburgx@gmail.com>
2012-12-19 23:59:21 +0100

    fin de la première partie user model

---
+++ .metadata/.plugins/com.aptana.ruby.core/-517239044/4/object.rb
@@ -0,0 +1,142 @@
+class Object < BasicObject
+  include Kernel
+
+  ARGF = ARGF
+  ARGV = []
+  ArgumentError = ArgumentError
+  Array = Array
+  BasicObject = BasicObject
+  Bignum = Bignum
+  Binding = Binding
+  Class = Class
+  Comparable = Comparable
+  Complex = Complex
+  Data = Data
+  Dir = Dir
+  ENV = {"GEM_PATH"=>"/Users/xaviervonaarburg/.rvm/gems/ruby-1.9.2-p320@rails3tutorial:/Users/xaviervonaarburg/.rvm/gems/ruby-1.9.2-p320@global", "APP_ICON_26125"=>"../Resources/aptana.icns", "SHLVL"=>"1", "__CF_USER_TEXT_ENCODING"=>"0x1F5:0:0", "rvm_version"=>"1.17.2 (master)", "__array_start"=>"0", "PWD"=>"/Applications/Aptana Studio 3/AptanaStudio3.app/Contents/MacOS", "rvm_prefix"=>"/Users/xaviervonaarburg", "LOGNAME"=>"xaviervonaarburg", "Apple_Ubiquity_Message"=>"/tmp/launch-UCHpwZ/Apple_Ubiquity_Message", "JAVA_STARTED_ON_FIRST_THREAD_26125"=>"1", "IRBRC"=>"/Users/xaviervonaarburg/.rvm/rubies/ruby-1.9.2-p320/.irbrc", "RUBY_VERSION"=>"ruby-1.9.2-p320", "SSH_AUTH_SOCK"=>"/tmp/launch-oS0PQP/Listeners", "MY_RUBY_HOME"=>"/Users/xaviervonaarburg/.rvm/rubies/ruby-1.9.2-p320", "SHELL"=>"/bin/bash", "TMPDIR"=>"/var/folders/kg/8g_zmrds4kgg7wc8gg1lp45w0000gn/T/", "rvm_bin_path"=>"/Users/xaviervonaarburg/.rvm/bin", "escape_flag"=>"1", "_first"=>"0", "PATH"=>"/Users/xaviervonaarburg/.rvm/gems/ruby-1.9.2-p320@rails3tutorial/bin:/Users/xaviervonaarburg/.rvm/gems/ruby-1.9.2-p320@global/bin:/Users/xaviervonaarburg/.rvm/rubies/ruby-1.9.2-p320/bin:/Users/xaviervonaarburg/.rvm/bin:/Applications/MAMP/bin/php/php5.3.6/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/git/bin", "_second"=>"1", "GEM_HOME"=>"/Users/xaviervonaarburg/.rvm/gems/ruby-1.9.2-p320@rails3tutorial", "APTANA_VERSION"=>"3.2.2.1343263605", "COMMAND_MODE"=>"unix2003", "DISPLAY"=>"/tmp/launch-zb8Jmw/org.macosforge.xquartz:0", "USER"=>"xaviervonaarburg", "com.apple.java.jvmTask"=>"JNI", "HOME"=>"/Users/xaviervonaarburg", "rvm_env_string"=>"ruby-1.9.2-p320@rails3tutorial", "rvm_path"=>"/Users/xaviervonaarburg/.rvm", "Apple_PubSub_Socket_Render"=>"/tmp/launch-k4gTbH/Render", "rvm_ruby_string"=>"ruby-1.9.2-p320", "LANG"=>"en_US.UTF-8"}
+  EOFError = EOFError
+  Encoding = Encoding
+  EncodingError = EncodingError
+  Enumerable = Enumerable
+  Enumerator = Enumerator
+  Errno = Errno
+  Etc = Etc
+  Exception = Exception
+  FALSE = false
+  FalseClass = FalseClass
+  Fiber = Fiber
+  FiberError = FiberError
+  File = File
+  FileTest = FileTest
+  FileUtils = FileUtils
+  Fixnum = Fixnum
+  Float = Float
+  FloatDomainError = FloatDomainError
+  GC = GC
+  Gem = Gem
+  Hash = Hash
+  IO = IO
+  IOError = IOError
+  IndexError = IndexError
+  Integer = Integer
+  Interrupt = Interrupt
+  Kernel = Kernel
+  KeyError = KeyError
+  LoadError = LoadError
+  LocalJumpError = LocalJumpError
+  Marshal = Marshal
+  MatchData = MatchData
+  Math = Math
+  Method = Method
+  Module = Module
+  Mutex = Mutex
+  NIL = nil
+  NameError = NameError
+  NilClass = NilClass
+  NoMemoryError = NoMemoryError
+  NoMethodError = NoMethodError
+  NotImplementedError = NotImplementedError
+  Numeric = Numeric
+  OUTPUT_PATH = "/Users/xaviervonaarburg/Sites/rails_projects/sample_app/.metadata/.plugins/com.aptana.ruby.core/-517239044/4/"
+  Object = Object
+  ObjectSpace = ObjectSpace
+  Proc = Proc
+  Process = Process
+  RUBY_COPYRIGHT = "ruby - Copyright (C) 1993-2012 Yukihiro Matsumoto"
+  RUBY_DESCRIPTION = "ruby 1.9.2p320 (2012-04-20 revision 35421) [x86_64-darwin12.2.1]"
+  RUBY_ENGINE = "ruby"
+  RUBY_PATCHLEVEL = 320
+  RUBY_PLATFORM = "x86_64-darwin12.2.1"
+  RUBY_RELEASE_DATE = "2012-04-20"
+  RUBY_REVISION = 35421
+  RUBY_VERSION = "1.9.2"
+  Random = Random
+  Range = Range
+  RangeError = RangeError
+  Rational = Rational
+  Regexp = Regexp
+  RegexpError = RegexpError
+  RubyVM = RubyVM
+  RuntimeError = RuntimeError
+  STDERR = IO.new
+  STDIN = IO.new
+  STDOUT = IO.new
+  ScriptError = ScriptError
+  SecurityError = SecurityError
+  Signal = Signal
+  SignalException = SignalException
+  StandardError = StandardError
+  StopIteration = StopIteration
+  String = String
+  Struct = Struct
+  Symbol = Symbol
+  SyntaxError = SyntaxError
+  SystemCallError = SystemCallError
+  SystemExit = SystemExit
+  SystemStackError = SystemStackError
+  TOPLEVEL_BINDING = #<Binding:0x007fcda186b980>
+  TRUE = true
+  Thread = Thread
+  ThreadError = ThreadError
+  ThreadGroup = ThreadGroup
+  Time = Time
+  TrueClass = TrueClass
+  TypeError = TypeError
+  UnboundMethod = UnboundMethod
+  ZeroDivisionError = ZeroDivisionError
+
+
+
+  protected
+
+
+  private
+
+  def dir_names(arg0)
+  end
+
+  def file_name(arg0)
+  end
+
+  def get_classes
+  end
+
+  def grab_instance_method(arg0, arg1)
+  end
+
+  def print_args(arg0)
+  end
+
+  def print_instance_method(arg0, arg1)
+  end
+
+  def print_method(arg0, arg1, arg2, arg3, arg4, *rest)
+  end
+
+  def print_type(arg0)
+  end
+
+  def print_value(arg0)
+  end
+
+end
---
+++ .metadata/.plugins/com.aptana.ruby.core/-517239044/4/threadgroup.rb
@@ -0,0 +1,24 @@
+class ThreadGroup < Object
+
+  Default = #<ThreadGroup:0x007fcda186b3e0>
+
+
+  def add(arg0)
+  end
+
+  def enclose
+  end
+
+  def enclosed?
+  end
+
+  def list
+  end
+
+
+  protected
+
+
+  private
+
+end
